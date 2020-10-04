0566a3a75f9f9be45a055cc8e26afe4bc255ad9b
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-10-04 15:48:36 -0400

    Initial commit

---
+++ scripts/generate_export.py
@@ -0,0 +1,4 @@
+"""
+This script handles pulling COMPLETE documents from a workflow and transforming
+the results into a csv format for further downstream processing.
+"""
---
+++ scripts/workflow_upload.py
@@ -0,0 +1,5 @@
+"""
+This script is meant to handle the uploading of invoices to the Indico model
+workflow.  Note that this script is where we would want to place logic for
+STP.
+"""
