5b4573d15cb74aa0f12ad2361d3370975fb1f5de
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-12-15 14:50:01 -0500

    make create teach task somewhat configurable

--- scripts/create_teach_task_from_labeled_dataset/create_teach_task.py
+++ scripts/create_teach_task_from_labeled_dataset/create_teach_task.py
@@ -1,17 +1,16 @@
 """
-CreateCrowdLabel ~ Creates teach task
+Create a labeled teach task from a labeled dataset using custom graphql queries
 
-GetCrowdLabelQuestionaire ~ Contains labelset id
+This is a work around until the ability to add data to teach tasks is supported
+in platform
 
-~submit labels~
-for row_index, label in snapshot:
-    SubmitQuestionarieExample
+Large assumption: data is uploaded as a csv with labeled data
 """
 import os
 import pandas as pd
 
 from indico import IndicoClient, IndicoConfig
-from indico.queries import GraphQLRequest, GetDataset
+from indico.queries import GraphQLRequest, GetDataset, CreateExport, DownloadExport
 
 from graphql_queries import (
     CREATE_TEACH_TASK,
@@ -20,7 +19,7 @@ from graphql_queries import (
 )
 
 
-def create_teach_task(client, dataset_id, teach_task_name):
+def create_teach_task(client, dataset_id, teach_task_name, classes):
     dataset = client.call(GetDataset(dataset_id))
     source_col_id = dataset.datacolumn_by_name("text").id
     variables = {
@@ -33,7 +32,7 @@ def create_teach_task(client, dataset_id, teach_task_name):
         "questions": [
             {
                 "type": "ANNOTATION",
-                "targets": labels,
+                "targets": classes,
                 "keywords": [],
                 "text": teach_task_name,
             }
@@ -59,8 +58,9 @@ def create_teach_task(client, dataset_id, teach_task_name):
 def label_teach_task(
     client, dataset_id, labelset_id, model_group_id, label_df, label_col
 ):
+    row_index_col = f"row_index_{dataset_id}"
     for _, row in label_df.iterrows():
-        row_index = row.row_index
+        row_index = row[row_index_col]
         labels = row[label_col]
         variables = {
             "datasetId": dataset_id,
@@ -79,72 +79,64 @@ def label_teach_task(
         )
 
 
-# NOTE, Configure
-HOST = os.getenv("INDICO_API_HOST", "cush.indico.domains")
+if __name__ == "__main__":
 
-# NOTE, Configure
-API_TOKEN_PATH = "../../indico_api_token.txt"
-with open(API_TOKEN_PATH) as f:
-    API_TOKEN = f.read()
+    # NOTE, Configure
+    HOST = os.getenv("INDICO_API_HOST", "cush.indico.domains")
 
-my_config = IndicoConfig(host=HOST, api_token=API_TOKEN, verify_ssl=False)
-INDICO_CLIENT = IndicoClient(config=my_config)
+    # NOTE, Configure
+    API_TOKEN_PATH = "../../indico_api_token.txt"
 
-DATASET_ID = 79
+    # boiler plate code in every script using indico calls
+    with open(API_TOKEN_PATH) as f:
+        API_TOKEN = f.read()
 
-labels = [
-    "Bill To Name",
-    "Client Name",
-    "Client PO#",
-    "Currency",
-    "Extended Amount",
-    "Freight Amount",
-    "Invoice Amount",
-    "Invoice Date",
-    "Invoice Line Description",
-    "Invoice Number",
-    "Remit to Address (City)",
-    "Remit to Address (State)",
-    "Remit to Address (Street)",
-    "Remit to Address (Zip Code)",
-    "Service Date",
-    "Service Location Name",
-    "Supplier Name",
-    "Tax Amount",
-]
+    my_config = IndicoConfig(host=HOST, api_token=API_TOKEN, verify_ssl=False)
+    INDICO_CLIENT = IndicoClient(config=my_config)
 
-labelset_id, model_group_id = create_teach_task(
-    INDICO_CLIENT, DATASET_ID, "Test GOS Invoice Task"
-)
-variables = {"id": teach_task_id}
-teach_task_stats = INDICO_CLIENT.call(
-    GraphQLRequest(query=GET_TEACH_TASK, variables=variables)
-)
-labelset_id = teach_task_stats["questionnaires"]["questionnaires"][0]["questions"][0][
-    "labelset"
-]["id"]
-model_group_id = teach_task_stats["questionnaires"]["questionnaires"][0]["questions"][
-    0
-]["modelGroupId"]
+    # NOTE: Configure
+    DATASET_ID = 79
+    
+    # NOTE: Configure - Dummy classes allow for the ability to have label tags in review
+    dummy_classes = []
 
-label_csv_filename = "/home/fitz/Documents/customers/cushman-wakefield/va-invoices/add_reviewed_data_project/data/merged_teach_tasks.csv"
-label_csv = pd.read_csv(label_csv_filename)
+    # NOTE: Configure - label names of all labels in the indico produced model
+    model_classes = [
+        "Bill To Name",
+        "Client Name",
+        "Client PO#",
+        "Currency",
+        "Extended Amount",
+        "Freight Amount",
+        "Invoice Amount",
+        "Invoice Date",
+        "Invoice Line Description",
+        "Invoice Number",
+        "Remit to Address (City)",
+        "Remit to Address (State)",
+        "Remit to Address (Street)",
+        "Remit to Address (Zip Code)",
+        "Service Date",
+        "Service Location Name",
+        "Supplier Name",
+        "Tax Amount",
+    ]
+    full_classes = dummy_classes + model_classes
 
-for _, row in label_csv.iterrows():
-    row_index = row.row_index
-    target = row.question
-    variables = {
-        "datasetId": DATASET_ID,
-        "labelsetId": labelset_id,
-        "labels": [
-            {
-                "rowIndex": row_index,
-                "target": target,
-            }
-        ],
-        "modelGroupId": model_group_id,
-    }
+    # NOTE: Configure
+    TEACH_TASK_NAME = "Test GOS Invoice Task v2.1"
 
-    INDICO_CLIENT.call(
-        GraphQLRequest(query=SUBMIT_QUESTIONNAIRE_EXAMPLE, variables=variables)
+    # NOTE: CONFIGURE
+    LABEL_COL = "labels"
+
+    labelset_id, model_group_id = create_teach_task(
+        INDICO_CLIENT, DATASET_ID, TEACH_TASK_NAME, full_classes
+    )
+
+    # Get labeled dataset
+    export = INDICO_CLIENT.call(CreateExport(dataset_id=DATASET_ID, file_info=True, wait=True))
+    labeled_data_df = INDICO_CLIENT.call(DownloadExport(export.id))    
+
+    label_teach_task(
+        INDICO_CLIENT, DATASET_ID, labelset_id, model_group_id, labeled_data_df, LABEL_COL
     )
