75c3d7bf3537375011bd0c683bb9afbf3ab20447
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-12-28 16:44:51 -0500

    clean up some config stuff for auto review script

--- scripts/auto_review/auto_review.py
+++ scripts/auto_review/auto_review.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 import pandas as pd
 from collections import defaultdict
 from indico.queries import (
@@ -8,48 +9,58 @@ from indico.queries import (
     SubmissionFilter,
     ListSubmissions,
     SubmitReview,
-    JobStatus
+    JobStatus,
 )
 from indico import IndicoClient, IndicoConfig
 
 
-# NOTE, Configure
-HOST = os.getenv("INDICO_API_HOST", "cush.indico.domains")
+# TODO: need to do config checks for error catching
+class Configuration:
+    def __init__(self, config):
+        self.host = config.get("host")
+        self.api_token_path = config.get("api_token_path")
+        self.workflow_id = config.get("workflow_id")
+        self.model_name = config.get("model_name")
 
-# NOTE, Configure
-API_TOKEN_PATH = "../../indico_api_token.txt"
-with open(API_TOKEN_PATH) as f:
-    API_TOKEN = f.read()
+    @classmethod
+    def from_yaml(cls, yaml_filepath):
+        with open(yaml_filepath) as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+        return cls(config)
 
-my_config = IndicoConfig(host=HOST, api_token=API_TOKEN, verify_ssl=False)
-INDICO_CLIENT = IndicoClient(config=my_config)
+
+# TODO: continue adding functionality
+class IndicoWrapper:
+    def __init__(self, host, api_token_path):
+        self.host = host
+        self.api_token_path = api_token_path
+        with open(api_token_path) as f:
+            self.api_token = f.read().strip()
+        my_config = IndicoConfig(
+            host=self.host, api_token=self.api_token, verify_ssl=False
+        )
+        self.indico_client = IndicoClient(config=my_config)
+
+    def get_submissions(self, workflow_id, status, retrieved):
+        sub_filter = SubmissionFilter(status=status, retrieved=retrieved)
+        complete_submissions = self.indico_client.call(
+            ListSubmissions(workflow_ids=[workflow_id], filters=sub_filter)
+        )
+        return complete_submissions
+
+
 EXCEPTION_STATUS = "PENDING_ADMIN_REVIEW"
-COMPLETE_STATUS = "COMPLETE"
-
-# NOTE, please configure this to the appropriate ID
-WORKFLOW_ID = 141
-
-# NOTE, please configure this to the appropriate model name
-MODEL_NAME = "GOS Invoice test model"
-
 AUTO_REVIEW_STATUS = "PENDING_AUTO_REVIEW"
 PENDING_REVIEW_STATUS = "PENDING_REVIEW"
-
-
-def get_submissions(client, workflow_id, status, retrieved):
-    sub_filter = SubmissionFilter(status=status, retrieved=retrieved)
-    complete_submissions = client.call(
-        ListSubmissions(workflow_ids=[workflow_id], filters=sub_filter)
-    )
-    return complete_submissions
+COMPLETE_STATUS = "COMPLETE"
 
 
 def auto_review_supplier_name(prediction):
     confidence = prediction["confidence"]["Supplier Name"]
-    if confidence > .98:
-        prediction[e"accepted"] = True
-    elif confidence < .5:
-        prediction["rejcted"] = True
+    if confidence > 0.98:
+        prediction["accepted"] = True
+    elif confidence < 0.5:
+        prediction["rejected"] = True
     return prediction
 
 
@@ -62,22 +73,40 @@ def auto_review_by_confidence(prediction, label, low_conf, high_conf):
     return prediction
 
 
+"""
+Idea for how to apply functions to a class 
+{"Supplier Name": [(fn, high_conf, low_conf), (fn, text_length, threshold)]}
+"""
 if __name__ == "__main__":
+
+    configuration_file = "./config.yaml"
+    config = Configuration.from_yaml(configuration_file)
+
+    indico_wrapper = IndicoWrapper(config.host, config.api_token_path)
+    indico_client = indico_wrapper.indico_client
+
     retrieved = False
-    auto_review_submissions = get_submissions(INDICO_CLIENT, WORKFLOW_ID, AUTO_REVIEW_STATUS, retrieved)
+
+    auto_review_submissions = indico_wrapper.get_submissions(
+        config.workflow_id, AUTO_REVIEW_STATUS, retrieved
+    )
 
     for submission in auto_review_submissions:
-        results = INDICO_CLIENT.call(RetrieveStorageObject(submission.result_file))
+        # TODO: add this call to IndicoWrapper
+        results = indico_client.call(RetrieveStorageObject(submission.result_file))
 
         updated_predictions = results["results"]["document"]["results"]
-        
-        for prediction in updated_predictions[MODEL_NAME]:                
+
+        for prediction in updated_predictions:
             if prediction["label"] == "Supplier Name":
                 auto_review_supplier_name(prediction)
-            
-        job = INDICO_CLIENT.call(SubmitReview(submission.id, changes=updated_predictions))
-    
-    pending_review_submissions = get_submissions(INDICO_CLIENT, WORKFLOW_ID, PENDING_REVIEW_STATUS, retrieved)
-    submission = pending_review_submissions[0]
-    results = INDICO_CLIENT.call(RetrieveStorageObject(submission.result_file))
-    assert "accepted" in results["results"]["document"]["results"][MODEL_NAME]
+
+        # TODO: add this call to IndicoWrapper
+        job = indico_client.call(
+            SubmitReview(submission.id, changes=updated_predictions)
+        )
+
+    # pending_review_submissions = get_submissions(INDICO_CLIENT, WORKFLOW_ID, PENDING_REVIEW_STATUS, retrieved)
+    # submission = pending_review_submissions[0]
+    # results = INDICO_CLIENT.call(RetrieveStorageObject(submission.result_file))
+    # assert "accepted" in results["results"]["document"]["results"][MODEL_NAME]
