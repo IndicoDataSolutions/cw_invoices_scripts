6826a18da2c79cf788f789ae420d0dc1bee79ca0
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2020-12-29 14:04:15 -0500

    odds and ends for auto_review

--- scripts/auto_review/auto_review.py
+++ scripts/auto_review/auto_review.py
@@ -1,15 +1,10 @@
-import os
 import yaml
-import pandas as pd
-from collections import defaultdict
 from indico.queries import (
     RetrieveStorageObject,
-    SubmissionResult,
-    UpdateSubmission,
     SubmissionFilter,
     ListSubmissions,
     SubmitReview,
-    JobStatus,
+    GetSubmission
 )
 from indico import IndicoClient, IndicoConfig
 
@@ -29,24 +24,38 @@ class Configuration:
         return cls(config)
 
 
-# TODO: continue adding functionality
 class IndicoWrapper:
     def __init__(self, host, api_token_path):
         self.host = host
         self.api_token_path = api_token_path
+
         with open(api_token_path) as f:
             self.api_token = f.read().strip()
+
         my_config = IndicoConfig(
             host=self.host, api_token=self.api_token, verify_ssl=False
         )
         self.indico_client = IndicoClient(config=my_config)
 
-    def get_submissions(self, workflow_id, status, retrieved):
-        sub_filter = SubmissionFilter(status=status, retrieved=retrieved)
+    def get_submission(self, workflow_id, submission_id):
+        return self.indico_client.call(GetSubmission(submission_id))
+
+    def get_submissions(self, workflow_id, submission_status=None, retrieved_flag=None):
+        sub_filter = SubmissionFilter(
+            status=submission_status, retrieved=retrieved_flag
+        )
         complete_submissions = self.indico_client.call(
             ListSubmissions(workflow_ids=[workflow_id], filters=sub_filter)
         )
         return complete_submissions
+
+    def get_workflow_output(self, submission):
+        return self.indico_client.call(RetrieveStorageObject(submission.result_file))
+
+    def submit_updated_review(self, submission, updated_predictions):
+        return self.indico_client.call(
+            SubmitReview(submission.id, changes=updated_predictions)
+        )
 
 
 EXCEPTION_STATUS = "PENDING_ADMIN_REVIEW"
@@ -79,21 +88,18 @@ Idea for how to apply functions to a class
 """
 if __name__ == "__main__":
 
-    configuration_file = "./config.yaml"
+    # TODO: make this an input arg
+    configuration_file = "/home/fitz/Documents/customers/cushman-wakefield/invoices/cw_invoices_scripts/scripts/auto_review/config.yaml"
     config = Configuration.from_yaml(configuration_file)
 
     indico_wrapper = IndicoWrapper(config.host, config.api_token_path)
-    indico_client = indico_wrapper.indico_client
-
-    retrieved = False
 
     auto_review_submissions = indico_wrapper.get_submissions(
-        config.workflow_id, AUTO_REVIEW_STATUS, retrieved
+        config.workflow_id, AUTO_REVIEW_STATUS, retrieved=False
     )
 
     for submission in auto_review_submissions:
-        # TODO: add this call to IndicoWrapper
-        results = indico_client.call(RetrieveStorageObject(submission.result_file))
+        results = indico_wrapper.get_workflow_output(submission)
 
         updated_predictions = results["results"]["document"]["results"]
 
@@ -101,12 +107,18 @@ if __name__ == "__main__":
             if prediction["label"] == "Supplier Name":
                 auto_review_supplier_name(prediction)
 
-        # TODO: add this call to IndicoWrapper
-        job = indico_client.call(
-            SubmitReview(submission.id, changes=updated_predictions)
-        )
+        # Note: this is a breaking call because we update the storage object
+        # need to be careful with handling this
+        job = indico_wrapper.submit_updated_review(submission, updated_predictions)
 
-    # pending_review_submissions = get_submissions(INDICO_CLIENT, WORKFLOW_ID, PENDING_REVIEW_STATUS, retrieved)
-    # submission = pending_review_submissions[0]
-    # results = INDICO_CLIENT.call(RetrieveStorageObject(submission.result_file))
-    # assert "accepted" in results["results"]["document"]["results"][MODEL_NAME]
+    pending_review_submissions = indico_wrapper.get_submissions(config.workflow_id, PENDING_REVIEW_STATUS)
+    submission = pending_review_submissions[0]
+    results = indico_wrapper.get_workflow_output(submission)
+    accepted_count = 0
+    rejected_count = 0
+    for prediction in results["results"]["document"]["results"][config.model_name]:
+        if "accepted" in prediction:
+            accepted_count += 1
+        if "rejected" in prediction:
+            rejected_count += 1
+    print(f"# accepted: {accepted_count}, # rejected: {rejected_count}")
