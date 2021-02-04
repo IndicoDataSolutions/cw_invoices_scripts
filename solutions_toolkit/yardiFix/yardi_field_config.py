489b48635a5f4649699c508602b82def7e869614
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2021-02-03 20:30:46 -0500

    initial yardi generate export file

---
+++ solutions_toolkit/yardiFix/yardi_field_config.py
@@ -0,0 +1,139 @@
+"""
+Field config configures the auto review functions that will apply to each class
+in a document's prediction
+
+It has the srtructure
+{
+    CLASS_NAME: [
+        {
+            "function": FUNCTION NAME,
+            "kwargs": {FUCNTION KEY WORDS},
+            "prediction_set": "single" or "all" ----> This determines whether the function works on the full label set or one at a time
+        }
+        .
+        .
+        .
+    ]
+}
+
+Note that the functions you apply need to have an appropriate mapping in
+the REVIEWERS dictionary in reviewer.py
+"""
+
+FIELD_CONFIG = {
+    "Check Amount": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Check Amount",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Check Date": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Check Date",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Check Number": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Check Number",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Deposit Amount": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Deposit Amount",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Deposit Date": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Deposit Date",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Withdrawal Amount": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Withdrawal Amount",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Withdrawal Date": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Withdrawal Date",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+        {
+            "function": "split_merged_values",
+            "kwargs": {},
+            "prediction_set": "all",
+        },
+    ],
+    "Ending Balance": [
+        {
+            "function": "reject_by_confidence",
+            "kwargs": {
+                "label": "Ending Balance",
+                "conf_threshold": 0.98
+            },
+            "prediction_set": "single",
+        },
+    ]
+}
