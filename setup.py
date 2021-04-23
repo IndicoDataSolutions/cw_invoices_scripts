d60d4e98535346f9cb633557defbd1addb1fae7b
Andrew Fitzgerald <andrew.fitzgerald@indico.io>
2021-04-23 14:49:04 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -25,5 +25,5 @@ setup(
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-    install_requires=["indico-client==4.5.0", "PyYAML", "tqdm"],
+    install_requires=["indico-client>=4.5.0", "PyYAML", "tqdm"],
 )
